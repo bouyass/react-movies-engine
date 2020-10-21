eb59c029a50a6b0597662ca704903e451e90e48a
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-10-21 06:35:32 +0200

    react movies app

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,10 +6,10 @@ function MovieCard(props) {
         <div className="card">
             <img src={props.picture} />
             <h2>{props.title}</h2>
-            <div>
-                <p><b>Disponible sur {props.available}</b> </p>
-                <button id="voir" onClick={() => {document.location.href = props.url}} > Voir le Film</button>
+            <div className="text">
+                <p id="available"><b>Disponible sur {props.available}</b> </p>
             </div>
+            <button id="voir" onClick={() => {document.location.href = props.url}} > Voir le Film</button>
             
         </div>
     )
